c149cda39a7ebe0da991fa8fd32a25a7fc50208b
Dami102 <damitigha@gmail.com>
2022-12-02 08:14:29 +0100

    Loans

--- src/pages/user/loan.js
+++ src/pages/user/loan.js
@@ -1,16 +1,32 @@
 import { useEffect, useState } from "react";
-import { getLoans } from "../../utils/requests";
+import { getLoans, getUserDetails } from "../../utils/requests";
 import { Flex } from "@chakra-ui/layout";
+import { useNavigate } from "react-router-dom";
 export default function Loan() {
   const [loans, setLoans] = useState([]);
+  const [user, setUser] = useState([]);
+  const navigate = useNavigate();
   useEffect(() => {
     (async () => {
-      const loanTypes = await getLoans();
-
-      setLoans(loanTypes);
+      const loans = await getLoans();
+      const userDet = await getUserDetails();
+      if (userDet.error) navigate("/login");
+      setUser(userDet);
+      setLoans(loans.loans);
     })();
-  }, [setLoans]);
+  }, [navigate]);
+  console.log(user.id);
   console.log(loans);
+  // const ll = loans.loans;
+  // console.log(ll[1].customer);
+  const userLoan = loans.reduce(
+    (acc, pos) =>
+      pos.customer.id === user.id
+        ? acc.push(pos.customer.id)
+        : acc.push("djdk"),
+    []
+  );
+  console.log(userLoan);
   return (
     <>
       <>
